36e67a662a88e4023ba5cfe6b252216bb6f53174
ibecoul <boulby9023@gmail.com>
2026-08-14 23:16:11 +0000

    ajout de l'architecture du projet

---
+++ lib/core/themes/app_colors.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // Couleur principale
+  static const Color primary = Color(0xFF6C63FF);
+
+  // Couleur secondaire
+  static const Color secondary = Color(0xFF8B85FF);
+
+  // Fond principal
+  static const Color background = Color(0xFFF8F9FC);
+
+  // Fond des cartes
+  static const Color surface = Colors.white;
+
+  // Texte principal
+  static const Color textPrimary = Color(0xFF1F2937);
+
+  // Texte secondaire
+  static const Color textSecondary = Color(0xFF6B7280);
+
+  // Bordures
+  static const Color border = Color(0xFFE5E7EB);
+
+  // Succès
+  static const Color success = Color(0xFF22C55E);
+
+  // Erreur
+  static const Color danger = Color(0xFFEF4444);
+
+  // Avertissement
+  static const Color warning = Color(0xFFF59E0B);
+
+  // Icônes
+  static const Color icon = Color(0xFF6B7280);
+
+  // Désactivé
+  static const Color disabled = Color(0xFFD1D5DB);
+
+  static const Color white = Colors.white;
+
+  static const Color black = Color(0xFF111827);
+}
---
+++ lib/shards/widgets/logo_wiget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LogoWiget extends StatelessWidget {
+  const LogoWiget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
