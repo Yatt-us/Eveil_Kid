000ce333f572f97b5a1c34e89b8bcb6dd612e731
ibecoul <boulby9023@gmail.com>
2026-08-17 21:03:23 +0000

    ajout initialisation  furebase  a la main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
+import 'package:eveilkid/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(home: Scaffold()));
-  // test
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("bonjour"));
+  }
 }
