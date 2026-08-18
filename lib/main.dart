08844661c29ca84f7713faf7cbd986d1668c4043
Amidou DIAGANA <diaganaamidou@gmail.com>
2026-08-18 17:14:04 +0000

    Initiation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:eveilkid/core/themes/AppTheme.dart';
+import 'package:eveilkid/features/parent/presentation/pages/accueil_parent.dart';
 import 'package:eveilkid/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +15,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("bonjour"));
+    return MaterialApp(
+      title: 'Eveil Kid',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      home: const AccueilParentPage(),
+    );
   }
 }
