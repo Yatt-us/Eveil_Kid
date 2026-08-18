ce8cbe253c441994384cb139b67275c318df2e16
ibecoul <boulby9023@gmail.com>
2026-08-18 14:07:30 +0000

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:eveilkid/core/constants/AppPadding.dart';
+import 'package:eveilkid/core/constants/AppTextStyles.dart';
 import 'package:eveilkid/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
@@ -13,6 +17,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("bonjour"));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Eveil Kid',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Eveil Kid')),
+        body: Padding(
+          padding: AppPadding.screenLarge,
+          child: SafeArea(
+            child: Column(
+              children: [Text('Bonjour', style: AppTextStyles.headingLarge)],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
